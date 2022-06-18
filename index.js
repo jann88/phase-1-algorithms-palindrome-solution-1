0dcf8d177216135574f799db004944746924d2e7
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-06-18 23:45:23 +0300

    commit tree 25173a63df3ca942572b67b1bcc700dfa7ba475b

--- index.js
+++ index.js
@@ -1,25 +1,75 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+    const wordArray = word.split("");
+    const reversedWordArray = wordArray.reverse();
+    const reversedWord = reversedWordArray.join("");
+
+    return reversedWord;
+
 }
 
+function isPalindrome(word) {
+    // Write your algorithm here
+    const reverseWord = reverseString(word);
+    if (word === reverseWord) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+
 /* 
-  Add your pseudocode here
+palindrome a word sentence spelt the same way both forward and backward ignoring puncuation cases and spaces.
+Add your pseudocode here
 */
 
 /*
-  Add written explanation of your solution here
-*/
+Add written explanation of your solution here
+
+const reverseWord = reverseString(word);
+if (word === reverseWord) {
+    return true;
+} else {
+    return false;
+    */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+    console.log("");
+
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("mom"));
+
+    console.log("");
+
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("abba"));
+
+    console.log("");
+
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("a"));
+
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("hi"));
+
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
